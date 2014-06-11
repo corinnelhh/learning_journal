d842abe6c9432cd95005d6f5bc63cbfa5dfda09e
Ian Auld <imauld@gmail.com>
2014-06-11 09:12:32 -0700

    Edited config file

--- config.py
+++ config.py
@@ -1,19 +1,18 @@
-import os
+from os import environ
 from passlib.hash import pbkdf2_sha256
 
-DATABASE = os.environ.get(
+DATABASE = environ.get(
     'DATABASE_URL', 'dbname=learning_journal'
 )
 
-ADMIN_USERNAME = os.environ.get(
+ADMIN_USERNAME = environ.get(
     'ADMIN_USERNAME', 'admin'
 )
 
-ADMIN_PASSWORD = os.environ.get(
+ADMIN_PASSWORD = environ.get(
     'ADMIN_PASSWORD', pbkdf2_sha256.encrypt('admin')
-
 )
 
-SECRET_KEY = os.environ.get(
+SECRET_KEY = environ.get(
     'FLASK_SECRET_KEY', 'C\x93d\xd8\xe0wcK\xcb\xc3\xd0\xab\x04\xf0\xd0?\xba\xfd\xa0\xbc\xca\xe4a\xd1aE\xcb\x03\xd7T[\xf8'
 )
