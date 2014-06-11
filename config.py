abadc551d0f317f74fa85480e65cbb59992389f6
Ian Auld <imauld@gmail.com>
2014-06-11 09:14:22 -0700

    Edited config file

--- config.py
+++ config.py
@@ -16,3 +16,7 @@ ADMIN_PASSWORD = environ.get(
 SECRET_KEY = environ.get(
     'FLASK_SECRET_KEY', 'C\x93d\xd8\xe0wcK\xcb\xc3\xd0\xab\x04\xf0\xd0?\xba\xfd\xa0\xbc\xca\xe4a\xd1aE\xcb\x03\xd7T[\xf8'
 )
+
+def this_is_a_test():
+    'This is a test'.split(' ')
+    return
