25951c85fbe4e86096494b720cc7ad1360c5a582
corinnelhh <corinne.hutchinson@gmail.com>
2014-06-13 16:09:31 -0700

    added pigmentation for code

--- journal.py
+++ journal.py
@@ -3,9 +3,6 @@ import os
 import datetime
 import psycopg2
 import markdown
-from pygments import highlight
-from pygments.lexers import PythonLexer
-from pygments.formatters import HtmlFormatter
 from flask import Flask, g, render_template, abort
 from flask import request, url_for, redirect, session
 from contextlib import closing
@@ -163,7 +160,7 @@ def get_single_entry(id):
 def show_entries():
     entries = get_all_entries()
     for entry in entries:
-        entry["text"] = markdown.markdown(entry["text"])
+        entry["text"] = markdown.markdown(entry["text"], extensions=["codehilite"])
     return render_template('list_entries.html', entries=entries)
 
 
